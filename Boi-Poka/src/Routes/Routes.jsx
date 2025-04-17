b28071af77f1f8ee747edf3307bb3c892275eee8
Moshiur-Rahman-01 <moshiur.ict24@gmail.com>
2025-04-18 00:38:49 +0600

    added

--- Boi-Poka/src/Routes/Routes.jsx
+++ Boi-Poka/src/Routes/Routes.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import App from "./App.jsx";
 import { createBrowserRouter } from "react-router";
+import Root from '../pages/Root/Root';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello World</div>,
+        //element: <div>Hello World</div>,
+        Component: Root,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                index: true,
+                path: '/',
+                Component: Home,
+            }
+        ]
     },
 ]);
